a7a9fed17b581753ed20deb2bb71386b8445def1
kuboyumi <121540707+kuboyumi@users.noreply.github.com>
2024-05-13 00:07:26 +0900

    GitHub OAuth (#3)
    * feat: database定義
    
    * feat: arctic導入
    
    * feat: ログインページ作成
    
    * feat: 認可URL作成
    
    * feat: callback検証を実装
    
    * feat: リクエスト検証を実装
    
    * feat: トップページ作成
    
    * fix: ミスの修正

---
+++ src/lib/db.ts
@@ -0,0 +1,22 @@
+import { Database } from 'bun:sqlite';
+
+export const db = new Database('main.db');
+
+db.exec(`create table if not exists user (
+  id text not null primary key,
+  github_id integer not null unique,
+  username text not null
+)`);
+
+db.exec(`create table if not exists session (
+  id text not null primary key,
+  expires_at integer not null,
+  user_id text not null,
+  foreign key (user_id) references user(id)
+)`);
+
+export interface DatabaseUser {
+  id: string;
+  github_id: number;
+  username: string;
+}
