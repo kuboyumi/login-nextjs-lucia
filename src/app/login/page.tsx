ad482458fc1bfdd4c4027484dc47de0e142f3056
kuboyumi <kuboyumi2@gmail.com>
2024-05-12 22:55:17 +0900

    feat: リクエスト検証を実装

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,11 @@
+import { redirect } from 'next/navigation';
+import { validateRequest } from "@/lib/auth";
+
 export default async function Page() {
+  const { user } = await validateRequest();
+  if (!user) {
+    return redirect("/login");
+  }
   return (
     <>
       <h1>Sign in</h1>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,10 @@ import { Lucia } from 'lucia';
 import { BunSQLiteAdapter } from '@lucia-auth/adapter-sqlite';
 import { db } from './db';
 import { GitHub } from 'arctic';
+import { cookies } from "next/headers";
+import { cache } from 'react';
+
+import type { Session, User } from 'lucia';
 
 const adapter = new BunSQLiteAdapter(db, {
   user: 'user',
@@ -39,3 +43,36 @@ export const github = new GitHub(
   process.env.GITHUB_CLIENT_ID!,
   process.env.GITHUB_CLIENT_SECRET!,
 );
+
+export const validateRequest = cache(
+  async (): Promise<{ user: User; session: Session } | {user: null; session: null }> => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch {}
+    return result;
+  }
+)
